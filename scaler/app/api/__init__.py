7853f66365922da550e7f7bb52eb96e081b77d7e
ahnguyen03 <ahnguyen@ualberta.ca>
2018-04-07 18:10:47 -0600

    graphing testing

--- scaler/app/api/__init__.py
+++ scaler/app/api/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file,render_template
+import pygal
 import sys
 from StringIO import StringIO
+import numpy as np
+import matplotlib.pyplot as plt
 import time
 import random
 from multiprocessing import Queue, Manager
+from pygal.style import DarkSolarizedStyle
 
 global timequeue
 global avg_response
@@ -28,25 +32,39 @@ def addmetric():
   else:
     return 'You did a get post on /metric'
 
+
 @app.route('/graphs', methods=['GET'])
 def plotstats():
   if request.method == 'GET':
-    return 'HELLO'
-#     plt.axis([0,200,0,200])
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111)
-#     plt.ion()
+    timeArray = []
+    someArray = []
+    title = 'reponse time vs elapsed time'
+    line_chart = pygal.Line(width=1200, height=600,explicit_size=True, title=title,style=DarkSolarizedStyle,disable_xml_declaration=True)
+    line_chart.title = 'Browser usage evolution (in %)'
+    line_chart.x_labels = [0,1,2,3,4,5,6,7,8,9,10]
+    for i in range(0,10):
+      y = random.randint(1,100)
+      someArray.append(y)
+      line_chart.add('Firefox', someArray)
+      line_chart.render_to_png('chart.png')
+      print("inside here")
+  return send_file('chart.png', mimetype='image/png')
+""" plt.axis([0,200,0,200])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+#   plt.ion()
+#   fig.show()
+    fig.canvas.draw()
 
-#     y = avg_response
-#     print(timeArray)
-#     for item in timeArray:
-#       print(item)
-#     ax.plot(timeArray, y, '-o')
-#     img = StringIO()
-#     fig.savefig(img)
-#     img.seek(0)
-#     return send_file(img, mimetype='image/png')
+    y = avg_response
+    print(timeArray)
+    for item in timeArray:
+      print(item)
+    ax.plot(timeArray, y, '-o')
+    img = StringIO()
+    fig.savefig(img)
+    img.seek(0)"""
 
-# @app.route('/printgraph')
-# def images():
-#   return render_template("templates/plot.html")
+@app.route('/printgraph')
+def images():
+   return render_template("templates/plot.html")
