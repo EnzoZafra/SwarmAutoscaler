55d24856649d38fb78dccb7de0157a26d69b6256
enzozafra <zafra@ualberta.ca>
2018-04-08 13:28:26 -0600

    add axis titles for plot

--- scaler/app/api/__init__.py
+++ scaler/app/api/__init__.py
@@ -35,27 +35,33 @@ def addmetric():
 def plotstats():
   if request.method == 'GET':
     times = [x for x in timeArray]
+    x_title = 'Elapsed time (sec)'
 
-    title = 'reponse time vs elapsed time'
-    line_chart = pygal.Line(width=1200, height=200,explicit_size=True, title=title,style=DarkSolarizedStyle,disable_xml_declaration=True)
+    title = 'Average Response Time vs Elapsed Time'
+    y_title = 'Response Time (sec)'
+    line_chart = pygal.Line(width=1200, height=200,explicit_size=True,
+                            title=title, x_title=x_title, y_title=y_title
+                            style=DarkSolarizedStyle,disable_xml_declaration=True)
     line_chart.x_labels = times
     avg_response_arr = [x for x in avg_response]
     line_chart.add('response time', avg_response_arr)
-    
-    title2 = 'workload vs elapsed time'
-    line_chart2 = pygal.Line(width=1200, height=200,explicit_size=True, title=title2,style=DarkSolarizedStyle,disable_xml_declaration=True)
+
+    title2 = 'Requests per second vs Elapsed Time'
+    y_title2 = 'Requests per second'
+    line_chart2 = pygal.Line(width=1200, height=200,explicit_size=True,
+                             title=title2, x_title=x_title, y_title=y_title2
+                             style=DarkSolarizedStyle,disable_xml_declaration=True)
     line_chart2.x_labels = times
     workload_arr = [x for x in workload]
     line_chart2.add('workload', workload_arr)
 
-    title3 = 'replication factor vs elapsed time'
-    line_chart3 = pygal.Line(width=1200, height=200,explicit_size=True, title=title3,style=DarkSolarizedStyle,disable_xml_declaration=True)
+    title3 = 'Number of Replications vs Elapsed Time'
+    y_title3 = 'Number of Replicas'
+    line_chart3 = pygal.Line(width=1200, height=200,explicit_size=True,
+                             title=title3, x_title=x_title, y_title=y_title3
+                             style=DarkSolarizedStyle,disable_xml_declaration=True)
     line_chart3.x_labels = times
     replications_arr = [x for x in replications]
     line_chart3.add('replication', replications_arr)
 
     return render_template("plot.html", line_chart=line_chart,line_chart2=line_chart2,line_chart3 = line_chart3)
-
-# @app.route('/printgraph')
-# def images():
-#    return render_template("html/plot.html")
