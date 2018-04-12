d4091b4d7f623784a6228746c21d65d08fe29c9c
enzozafra <zafra@ualberta.ca>
2018-04-12 17:04:33 -0600

    bell curve

--- bellcurve.sh
+++ bellcurve.sh
@@ -30,11 +30,3 @@ kill -HUP $PID
 python3 client/http_client.py 10.1.0.138 15 0.5 & PID=$!
 sleep 1m
 kill -HUP $PID
-
-python3 client/http_client.py 10.1.0.138 10 0.5 & PID=$!
-sleep 1m
-kill -HUP $PID
-
-python3 client/http_client.py 10.1.0.138 10 1 & PID=$!
-sleep 30s
-kill -HUP $PID
